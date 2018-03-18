ff1f9522569bc19e280db75c2131e0f2619defe3
avijit sarkar <avisofficeone@gmail.com>
2018-03-18 13:22:02 +0530

    enty file changed to wt-plug.php

---
+++ wt-plug.php
@@ -0,0 +1,11 @@
+<?php
+/*
+    Plugin Name: wt-plug
+    Plugin URI: https://github.com/insetavijit/wt-plug.git
+    Description: A simple Clean minimalistc mordern wordpress plugin devlopment workfollow - seed 
+    Version: 1.0.0
+    Author: Avijit sarkar
+    Author URI: https://github.com/insetavijit/
+    License: GPLv2 or later
+    Text Domain: inset
+*/
